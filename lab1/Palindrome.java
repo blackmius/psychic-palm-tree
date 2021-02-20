6b949be37b95c1ce0bdd5ceae638b8ca50afb176
d.terlyakhin <blackmius@gmail.com>
2021-02-20 19:17:37 +0300

    Лабораторная 1

---
+++ lab1/Palindrome.java
@@ -0,0 +1,23 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            String s = args[i];
+            if (isPalindrome(s))
+                System.out.printf("%s это палиндром\n", s);
+            else
+                System.out.printf("%s это не палиндром\n", s);
+        }
+    }
+    
+    public static String reverse(String s) {
+        String result = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            result += s.charAt(i);
+        }
+        return result;
+    }
+    
+    public static boolean isPalindrome(String s) {
+        return s.equals(reverse(s));
+    }
+}
